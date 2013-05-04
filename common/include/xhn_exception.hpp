7ed59b8d95bc2beacace8a063cfd3d45e8039421
xhnsworks <xhnsworks@gmail.com>
2013-05-04 12:13:12 +0800

    解决osx下直接运行app崩溃的bug，同时修复一堆warning

---
+++ common/include/xhn_exception.hpp
@@ -0,0 +1,14 @@
+//
+//  xhn_exception.hpp
+//  ecg
+//
+//  Created by 徐 海宁 on 13-5-3.
+//  Copyright (c) 2013年 徐 海宁. All rights reserved.
+//
+
+#ifndef ecg_xhn_exception_hpp
+#define ecg_xhn_exception_hpp
+
+
+
+#endif
