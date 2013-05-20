f834bf45dd9185796a6a561cd56e2817f7afb59f
xhnsworks <xhnsworks@gmail.com>
2013-05-20 18:04:58 +0800

    删除一些c代码

--- renderer/新建文件夹/include/viewport.h
+++
@@ -1,9 +0,0 @@
-#ifndef VIEWPORT_H
-#define VIEWPORT_H
-#include "common.h"
-#include "etypes.h"
-#include <windows.h>
-struct _viewport;
-typedef struct _viewport Viewport;
-API_EXPORT void ViewPort_Init(HINSTANCE hInstance, uint _width, uint _height);
-#endif
--- renderer/新建文件夹/src/shader_node_base.c
+++
@@ -1,7 +0,0 @@
-#include "shader_node_base.h"
-#include "emem.h"
-void ShaderNodeBase_delete(ShaderNodeBase _self, const char* _file, uint _line)
-{
-    _self->shader_node_dest_proc(_self, _file, _line);
-    Mfree(_self);
-}
